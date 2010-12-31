17ed5ab67fa40e1edef21e6c57c4b4fea2898d3b
Gilles Crofils <gilles@secondbureau.com>
2010-12-31 19:29:37 +0100

    #99

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
 private  
    
    def meta_defaults
-     @page_title = 'SecondBureau'
+     @page_title = 'Retail, Web & Innovation'
      @page_keywords = %w[multicanal web retail technologie acquisition conversion rétention parcours_client offre prix promotion logistique organisation labs référencement stratégie marketing].shuffle
-     @page_description = 'Secondbureau'
+     @page_description = 'SecondBureau, spécialiste des problématiques du commerce en ligne, allie expertise web et savoir-faire dans la distribution pour révolutionner le développement de vos activités sur Internet.'
    end
 end
