4bf8fc0e17101c4704a2b9be1c6b68d1401f1dd0
Gilles Crofils <gilles@secondbureau.com>
2010-12-31 17:43:15 +0100

    #99

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   
   def index
+    images = Dir["#{Rails.root.join('public', 'images', 'wordle')}/*"].select { |file| /(png)$/ =~ file }
+    image = File.basename(images[rand(images.count)])
+    @bodycolor = "color#{image.split('-')[0]}"
+    @wordle= File.join('wordle', image)
+    
   end
   
 end
